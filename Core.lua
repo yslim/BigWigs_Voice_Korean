0272a967faf35f209590724f8d778b8130cf44df
yslim <yslim@mulbul.com>
2020-07-20 14:50:26 +0900

    Transform to Korean Voice

--- Core.lua
+++ Core.lua
@@ -1,4 +1,3 @@
-
 local name, addon = ...
 
 --------------------------------------------------------------------------------
@@ -13,14 +12,14 @@ addon.SendMessage = BigWigsLoader.SendMessage
 -- Event Handlers
 --
 
-local path = "Interface\\AddOns\\BigWigs_Voice\\Sounds\\%s.ogg"
-local pathYou = "Interface\\AddOns\\BigWigs_Voice\\Sounds\\%sy.ogg"
+local path = "Interface\\AddOns\\BigWigs_Voice_Korean\\Sounds\\%s.ogg"
+local pathYou = "Interface\\AddOns\\BigWigs_Voice_Korean\\Sounds\\%sy.ogg"
 local function handler(event, module, key, sound, isOnMe)
-	local success = PlaySoundFile(format(isOnMe and pathYou or path, tostring(key)), "Master")
-	if not success then
-		addon:SendMessage("BigWigs_Sound", module, key, sound) 
-	end
+   local success = PlaySoundFile(format(isOnMe and pathYou or path, tostring(key)), "Master")
+   if not success then
+      addon:SendMessage("BigWigs_Sound", module, key, sound)
+   end
 end
 
-BigWigsLoader.RegisterMessage(addon, "BigWigs_Voice", handler)
+BigWigsLoader.RegisterMessage(addon, "BigWigs_Voice_Korean", handler)
 BigWigsAPI.RegisterVoicePack("temp")
